1a89b78377e4d741b14063b665dfc66116397c78
asieduernest12 <asieduernest@yahoo.co.uk>
2023-10-29 16:32:46 -0400

    Update(ui)

--- src/PortalledContainer.tsx
+++ src/PortalledContainer.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Stack, Step, StepContent, StepLabel, Stepper } from "@mui/material";
+import React, { useRef } from "react";
+import { Box, Divider, Stack, Step, StepContent, StepLabel, Stepper } from "@mui/material";
 
 const formSteps = [
   { index: 0, label: "form 0", component: <>form 0 component</> },
@@ -11,13 +11,13 @@ const formSteps = [
 export const PortalledContainer = () => {
   const container = useRef(null)
   return (
-    <Stack direction="row">
+    <Stack direction="row" className='PortalledContainer' sx={{width:'100%',height:'100%'}} spacing={2}>
       <Box
         className="sidepanel-showStepperIndexes"
         sx={{ flex: "0 0 20%" }}
       >
         {/* show steppers list */}
-        <Stepper>
+        <Stepper orientation="vertical">
           {formSteps.map(step=> (
             <Step>
               <StepLabel>{step.label}</StepLabel>
@@ -26,6 +26,9 @@ export const PortalledContainer = () => {
           ))}
         </Stepper>
       </Box>
+      
+      <Divider orientation="vertical" />
+
       <Box className="portalledComponent" sx={{ flex: "0 1" }}>
         <h2>Portal container</h2>
 
